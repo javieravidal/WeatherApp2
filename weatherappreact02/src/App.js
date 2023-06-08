6a1d4f9433fb374accb4d020b433a7d7fe45acbf
javieravidal <126521755+javieravidal@users.noreply.github.com>
2023-06-07 20:30:12 -0400

    Added API and weather icons

--- weatherappreact02/src/App.js
+++ weatherappreact02/src/App.js
@@ -1,12 +1,39 @@
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <div className="container">
+        <Weather defaultCity="New York" />
+        </div>
+        <footer className="footer">
+          This project was coded by{" "}
+          <a
+            href="https://www.javieravidal.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Javiera Vidal
+          </a> 
+          and is&#32; 
+          <a
+            href="https://github.com/javieravidal/ReactWeatherApp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub 
+          </a> &#32;and 
+          <a
+            href="https://cheery-shortbread-8440df.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      
     </div>
   );
 }
-
-export default App;
--- weatherappreact02/src/index.js
+++ weatherappreact02/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Container from './Container.js';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+
+
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <Container />
-  </React.StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
-
